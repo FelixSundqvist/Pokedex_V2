cd4480c9b23deebe32826a9e7312eecd5bd9c570
FelixSundqvist <felix.sundqvist@outlook.com>
2019-07-22 23:32:33 +0300

    Updates

---
+++ static/js/3.fac21dc4.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{159:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),o=t(130),i=Object(o.a)(function(e){return{root:{display:"flex",justifyContent:"center",color:"white",textTransform:"capitalize"},eggGroup:{padding:2*e.spacing(),margin:e.spacing(),border:"2px solid white"}}});n.default=function(e){var n=e.eggGroups,t=i();return r.a.createElement("div",{className:t.root},n.map(function(e){return r.a.createElement("div",{className:t.eggGroup,key:e.name},e.name)}))}}}]);
+//# sourceMappingURL=3.fac21dc4.chunk.js.map
